8d21f1e86690c681e1dffac6ede2df231a78fdfe
TheDeathlyCow <thedeathlycow@gmail.com>
2024-11-25 19:26:53 -1000

    remove always-cancelling call

--- src/main/java/com/github/thedeathlycow/frostiful/mixins/block/PowderSnowWalkableMixin.java
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/block/PowderSnowWalkableMixin.java
@@ -5,33 +5,26 @@ import net.minecraft.block.PowderSnowBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.EntityTypeTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PowderSnowBlock.class)
-public class PowderSnowWalkableMixin {
-
-
+public abstract class PowderSnowWalkableMixin {
     @Inject(
             method = "canWalkOnPowderSnow",
             at = @At("HEAD"),
             cancellable = true
     )
-    private static void canWalkOnPowderSnowBootTag(Entity entity, CallbackInfoReturnable<Boolean> cir) {
-        if (!entity.getType().isIn(EntityTypeTags.POWDER_SNOW_WALKABLE_MOBS) && (entity instanceof LivingEntity livingEntity)) {
-
+    private static void checkPowderSnowWalkableItemTag(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (entity instanceof LivingEntity livingEntity) {
             for (ItemStack stack : livingEntity.getArmorItems()) {
                 if (!stack.isEmpty() && stack.isIn(FItemTags.POWDER_SNOW_WALKABLE)) {
                     cir.setReturnValue(true);
                     return;
                 }
             }
-
-            cir.setReturnValue(false);
         }
     }
-
 }
