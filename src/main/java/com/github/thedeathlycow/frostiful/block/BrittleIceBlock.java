2f5aa94284427a67b307ad7381343791f2149ac5
TheDeathlyCow <thedeathlycow@gmail.com>
2024-12-06 18:44:07 -1000

    cracking in property

--- src/main/java/com/github/thedeathlycow/frostiful/block/BrittleIceBlock.java
+++ src/main/java/com/github/thedeathlycow/frostiful/block/BrittleIceBlock.java
@@ -1,14 +1,15 @@
 package com.github.thedeathlycow.frostiful.block;
 
+import com.github.thedeathlycow.frostiful.registry.FBlockProperties;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class BrittleIceBlock extends Block {
-    public static final EnumProperty<BrittleIceState> CRACKING = BrittleIceState.CRACKING;
+    public static final IntProperty CRACKING = FBlockProperties.CRACKING;
 
     public BrittleIceBlock(Settings settings) {
         super(settings);
---
+++ src/main/java/com/github/thedeathlycow/frostiful/registry/FBlockProperties.java
@@ -0,0 +1,12 @@
+package com.github.thedeathlycow.frostiful.registry;
+
+import net.minecraft.state.property.IntProperty;
+
+public final class FBlockProperties {
+
+    public static final IntProperty CRACKING = IntProperty.of("cracking", 0 , 3);
+
+    private FBlockProperties() {
+
+    }
+}
