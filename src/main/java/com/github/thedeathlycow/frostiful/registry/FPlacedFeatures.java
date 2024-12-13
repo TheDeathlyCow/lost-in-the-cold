b49772cfec9532866809ee164c83b37b53a54829
TheDeathlyCow <thedeathlycow@gmail.com>
2024-12-13 05:21:20 -1000

    make worldgen place frozen state

--- src/main/java/com/github/thedeathlycow/frostiful/registry/FPlacedFeatures.java
+++ src/main/java/com/github/thedeathlycow/frostiful/registry/FPlacedFeatures.java
@@ -6,7 +6,6 @@ import net.fabricmc.fabric.api.biome.v1.BiomeModification;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
-import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBiomeTags;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.world.gen.GenerationStep;
@@ -16,7 +15,7 @@ public class FPlacedFeatures {
 
     public static final RegistryKey<PlacedFeature> SUN_LICHEN_COVERED_ROCK = of("sun_lichen_covered_rock");
     public static final RegistryKey<PlacedFeature> ICICLE_CLUSTER = of("icicle_cluster");
-    public static final RegistryKey<PlacedFeature> BRITTLE_ICE_DISK = of("brittle_ice_disk");
+    public static final RegistryKey<PlacedFeature> BRITTLE_ICE = of("brittle_ice");
 
     public static void initialize() {
         Frostiful.LOGGER.debug("Initialized Frostiful placed features");
@@ -47,11 +46,11 @@ public class FPlacedFeatures {
 
         modification.add(
                 ModificationPhase.ADDITIONS,
-                BiomeSelectors.tag(ConventionalBiomeTags.IS_AQUATIC_ICY),
+                BiomeSelectors.tag(FHasFeatureTags.BRITTLE_ICE),
                 (biomeSelectionContext, biomeModificationContext) -> {
                     biomeModificationContext.getGenerationSettings().addFeature(
                             GenerationStep.Feature.TOP_LAYER_MODIFICATION,
-                            FPlacedFeatures.BRITTLE_ICE_DISK
+                            FPlacedFeatures.BRITTLE_ICE
                     );
                 }
         );
