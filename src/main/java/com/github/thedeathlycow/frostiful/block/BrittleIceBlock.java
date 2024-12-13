58a4906901f4f545f367b363e567939750c30f1b
TheDeathlyCow <thedeathlycow@gmail.com>
2024-12-13 05:20:01 -1000

    actually use a frozen state

--- src/main/java/com/github/thedeathlycow/frostiful/block/BrittleIceBlock.java
+++ src/main/java/com/github/thedeathlycow/frostiful/block/BrittleIceBlock.java
@@ -2,39 +2,32 @@ package com.github.thedeathlycow.frostiful.block;
 
 import com.github.thedeathlycow.frostiful.registry.FBlockProperties;
 import com.mojang.serialization.MapCodec;
-import net.minecraft.block.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TranslucentBlock;
 import net.minecraft.entity.Entity;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.IntProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
-
-public class BrittleIceBlock extends TranslucentBlock implements Waterloggable {
+public class BrittleIceBlock extends TranslucentBlock {
 
     public static final MapCodec<BrittleIceBlock> CODEC = createCodec(BrittleIceBlock::new);
 
     public static final IntProperty CRACKING = FBlockProperties.CRACKING;
 
-    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+    public static final BooleanProperty FROZEN = FBlockProperties.FROZEN;
 
     public BrittleIceBlock(Settings settings) {
         super(settings);
         this.setDefaultState(
                 this.getDefaultState()
                         .with(CRACKING, 0)
-                        .with(WATERLOGGED, false)
+                        .with(FROZEN, false)
         );
     }
 
@@ -46,7 +39,7 @@ public class BrittleIceBlock extends TranslucentBlock implements Waterloggable {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
-        builder.add(CRACKING, WATERLOGGED);
+        builder.add(CRACKING, FROZEN);
     }
 
     @Override
@@ -62,41 +55,5 @@ public class BrittleIceBlock extends TranslucentBlock implements Waterloggable {
             return;
         }
         BrittleIce.crack(this, state, world, pos, random);
-    }
-
-    @Override
-    protected FluidState getFluidState(BlockState state) {
-        return Boolean.TRUE.equals(state.get(WATERLOGGED))
-                ? Fluids.WATER.getStill(false)
-                : super.getFluidState(state);
-    }
-
-    @Nullable
-    @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        BlockState base = super.getPlacementState(ctx);
-
-        if (base == null) {
-            return null;
-        }
-
-        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
-        return base.with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
-    }
-
-    @Override
-    protected BlockState getStateForNeighborUpdate(
-            BlockState state,
-            Direction direction,
-            BlockState neighborState,
-            WorldAccess world,
-            BlockPos pos,
-            BlockPos neighborPos
-    ) {
-        if (Boolean.TRUE.equals(state.get(WATERLOGGED))) {
-            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
-        }
-
-        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 }
