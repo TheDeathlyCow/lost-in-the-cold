07b6599f6985594ea54081be28c0e5fccb38341a
TheDeathlyCow <thedeathlycow@gmail.com>
2025-01-04 02:47:56 -1000

    custom sun lichen discharge advancement criterion

---
+++ src/main/java/com/github/thedeathlycow/frostiful/registry/FCriteria.java
@@ -0,0 +1,26 @@
+package com.github.thedeathlycow.frostiful.registry;
+
+import com.github.thedeathlycow.frostiful.Frostiful;
+import com.github.thedeathlycow.frostiful.entity.advancement.SunLichenDischargeCriterion;
+import net.minecraft.advancement.criterion.Criterion;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
+public final class FCriteria {
+    public static final SunLichenDischargeCriterion SUN_LICHEN_DISCHARGE = register(
+            "sun_lichen_discharge",
+            new SunLichenDischargeCriterion()
+    );
+
+    public static void initialize() {
+        Frostiful.LOGGER.debug("Initialized Frostiful advancement criteria");
+    }
+
+    public static <T extends Criterion<?>> T register(String name, T criterion) {
+        return Registry.register(Registries.CRITERION, Frostiful.id(name), criterion);
+    }
+
+    private FCriteria() {
+
+    }
+}
